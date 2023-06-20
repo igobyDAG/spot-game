e52db04116063fca0e89d04745168133d6669c6b
dgonzalez-ias <dgonzalez@integralads.com>
2023-06-20 12:47:45 -0600

    implemented useSetGameHook into App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,11 @@
-import { useState } from "react";
-import { useEffect } from "react";
+import useSetGame from "./hooks/useSetGame";
 
-import { Dot } from "./Dot";
-import { getRandomWholeNumber } from "./utilities";
-import { getRandomRGB } from "./utilities";
+import { Dot } from "./components/Dot";
 
 import "./App.css";
 
 function App() {
-  const [color, setColor] = useState<string>("");
-  const [answers, setAnswers] = useState<string[]>([]);
-  const [correctGuess, setCorrectGuess] = useState<boolean | undefined>(
-    undefined
-  );
-  // const [dotsArraySize, setDotsArraySize] = useState(0)  // todo: assing this through ui
-
-  const dotsArray = 9;
-
-  const createColors = (size: number) => {
-    // This function is to be used in useEffect to initialize the colors data
-    const [rgbValue, red, green, blue] = getRandomRGB();
-    let colorsArray: string[] = new Array(size - 1).fill(rgbValue);
-    const offsetValue = 40; // how much will the color differ todo: Make it changeable based on if the game is easy / hard
-    const newColor = `rgb(${(red as number) - offsetValue},${
-      (green as number) - offsetValue
-    },${(blue as number) - offsetValue})`;
-
-    const randomIndexPosition = getRandomWholeNumber(0, size + 1);
-    colorsArray[randomIndexPosition] = newColor;
-
-    setColor(newColor);
-    setAnswers(colorsArray);
-  };
-
-  useEffect(() => {
-    createColors(dotsArray);
-  }, []);
-
-  const checkAnswer = (value: string) => {
-    if (value === color) {
-      setCorrectGuess(true);
-      createColors(dotsArray);
-    } else {
-      setCorrectGuess(false);
-    }
-  };
+  const {answers, checkAnswer, correctGuess} = useSetGame()
 
   return (
     <>
